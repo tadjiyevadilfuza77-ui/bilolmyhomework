d3532d5a52d51ec1aa3fb07e63fb29d08ad93694
tadjiyevadilfuza77-ui <tadjiyevadilfuza77@gmail.com>
2025-09-20 10:03:38 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+// let son= Math.round(4.5)
+// let son= Math.floor(4.5)
+// let son= Math.ceil(4.5)
+// let son= Math.sqrt(4.5)
+// console.log(son)
+
+
+
+
+
+// console.log(Math.max(2,3,1));
+// console.log(Math.min(67,7,13));
+
+
+
+// let randonNumber=Math.floor(Math.random()*100)
+// console.log(randonNumber)
+
+alert("Sizda hozir random sonlar chiqishni boshleydi😉")
+let random = Math.floor(Math.random() * 80)
+alert(`random orqali chiqqan son: ${random}`)
+let kopaytma = random * 3
+let bolinma = kopaytma / 3
+confirm(`biz random sonni 2 ko'paytirib ${kopaytma} Sonini chiqardik !`)
+confirm(`va shu sonni 2 ga bo'lib: ${bolinma} sonini chiqardik!`)
+alert(`Random orqali chiqqan son: ${random} edi \n biz uni 2ga kopaytirib ${kopaytma} sonini chiqardik, \n va shu sonni 2ga bolib: ${bolinma} sonini chiqardik.`)
